3bd28265957dc3b3d2ad28cb49867e261e3df396
yoshixi <davidchenyuhe@proton.me>
2024-10-28 19:24:25 -0400

    backend and frontend organization

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.json());
+app.listen(3000, () => {
+  console.log("server is running");
+})
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.status(200).sendFile(path.join(__dirname,'..', 'client', 'index.html'))
+})
