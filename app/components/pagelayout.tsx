466aafea0b26ad96d8dbf58c35121992f22b458c
yoshixi <digitalyoshixi@proton.me>
2024-12-01 16:42:45 -0500

    fixing divs

--- app/components/pagelayout.tsx
+++ app/components/pagelayout.tsx
@@ -6,22 +6,23 @@ export default function PageLayout( { children }  : {children : ReactNode} ){
         <div className="bg-blue-500 h-screen w-screen">
             <div className="h-screen flex flex-row"> 
                 {/* Profile Sidebar */}
-                <div className="w-1/5 mx-5 justify-center items-center">
-                    <div className="bg-white rounded-lg my-[5%]  h-[20%] mx-auto">
+                <div className="flex flex-col w-1/5 mx-[1%]">
+                    <div className="bg-white rounded-lg my-[2%] h-[20%] flex-none mx-auto">
+                        H
                     </div> 
-                    <div className="bg-white rounded-lg w-full h-[67%]">
-                        He
-                    </div> 
-                </div>
-                <div className="w-3/5">
-                    <div className="font-acme font-light text-4xl text-neutral-900 text-center h-[6%] my-[1%] flex bg-white w-[50%] mx-auto justify-center items-center">
-                          Yoshixis Web
-                    </div>
-                    <div className="font-sourGummy rounded-lg bg-white shadow-md w-full h-[90%] text-center"> 
-                        {children}
+                    <div className='rounded-lg flex-grow p-5 bg-white mb-[2%]'>
+                        H
                     </div>
                 </div>
-                <div className="bg-white w-1/5 mx-5  flex justify-center items-center">
+                <div className="flex flex-col w-3/5">
+                    <div className="rounded-lg font-acme font-light text-4xl text-neutral-900 text-center flex-none h-[6%] w-[50%] mx-auto bg-white my-[2%]">
+                        Yoshixis Web
+                    </div>
+                    <div className='rounded-lg flex-grow p-5 bg-white mb-[2%]'>
+                        H
+                    </div>
+                </div>
+                <div className="bg-white w-1/5 mx-[1%]  flex justify-center items-center">
                     Hello
                 </div>
             </div>
