016bfe5359696865781b0031d5516237b9a7be69
yoshixi <digitalyoshixi@proton.me>
2024-12-22 21:27:17 -0500

    no image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 import createMDX from '@next/mdx'
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  // Add the output property for static export
+  output: 'export',
   // Optionally, add any other Next.js config below
+  images : {unoptimized : true},
 }
- 
+
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
 })
- 
+
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig)
