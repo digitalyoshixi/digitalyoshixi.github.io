8c73de09d44370aaebdb30063462952309b1e244
yoshixi <davidchenyuhe@proton.me>
2024-12-25 18:14:40 -0500

    shillings - changing text sizes

--- mdx-components.js
+++ mdx-components.js
@@ -1,8 +1,8 @@
 export function useMDXComponents(components) {
   return {
     ...components,
-    h2 : ({ children }) => <h2 className="text-1xl">{children}</h2>,
-    h1 : ({ children }) => <h1 className="text-2xl text-green-500">{children}</h1>,
+    h1 : ({ children }) => <h1 className="text-4xl text-green-500">{children}</h1>,
+    h2 : ({ children }) => <h2 className="text-3xl">{children}</h2>,
     strong: ({ children }) => <strong className="text-red-500">{children}</strong>,
     em: ({ children }) => <em className="text-blue-500">{children}</em>
   }
