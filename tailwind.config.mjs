358c527e7b29a7ab311128973206cd29c7874b50
yoshixi <davidchenyuhe@proton.me>
2024-12-25 20:13:15 -0500

    working mdx

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,8 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./mdx-components.js"
+
   ],
   theme: {
     extend: {
@@ -20,5 +22,5 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
