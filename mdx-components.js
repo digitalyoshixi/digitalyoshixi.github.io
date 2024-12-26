ab67b34644835cce506f6a4301ee7bdc6cb32c47
yoshixi <davidchenyuhe@proton.me>
2024-12-25 20:54:31 -0500

    homepage

--- mdx-components.js
+++ mdx-components.js
@@ -4,6 +4,10 @@ export function useMDXComponents(components) {
     h1 : ({ children }) => <h1 className="text-4xl text-green-500">{children}</h1>,
     h2 : ({ children }) => <h2 className="text-3xl">{children}</h2>,
     strong: ({ children }) => <strong className="text-red-500">{children}</strong>,
-    em: ({ children }) => <em className="text-blue-500">{children}</em>
+    em: ({ children }) => <em className="text-blue-500">{children}</em>,
+    p: ({ children }) => <p className="text-black">{children}</p>,
+    li: ({ children }) => <li className="text-black">{children}</li>,
+    a: ({ children, href }) => <a href={href} className="text-blue-400 underline">{children}</a>,
+    
   }
 }
