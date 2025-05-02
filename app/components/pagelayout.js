bb16edbd5394fd762110e75c9b5821be116125bd
yoshixi <davidchenyuhe@proton.me>
2025-05-02 18:44:54 -0400

    test push

--- app/components/pagelayout.js
+++ app/components/pagelayout.js
@@ -65,7 +65,7 @@ export default function PageLayout( { title, children } ){
                             <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-purple-500" href="/excalidraw">Excalidraw Rice</Link>
                             <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-red-500" href="">Invidious</Link>
                             <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-orange-500" href="">Redlib</Link>
-                            <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-green-500" href="">Personal CTFd</Link>
+                            <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-green-500" href="">Personal CTFs</Link>
                         </div> 
                         <div className="flex-grow font-sourgummy text-white bg-black border-2 rounded-lg w-full mb-[6%]">
                             
