cfc0a86e12ec0f6f00f9f405b1d93681e38ce241
yoshixi <davidchenyuhe@proton.me>
2025-05-02 18:05:46 -0400

    update vulns and new stuff

--- app/components/pagelayout.js
+++ app/components/pagelayout.js
@@ -60,8 +60,12 @@ export default function PageLayout( { title, children } ){
                         </div> 
                         <div className="flex-none font-sourgummy text-white bg-black border-2 rounded-lg w-full h-[40%] my-[6%]">
                             <div className="text-center">
-                                Games
+                                Selfhosted Services / Tools / Toys
                             </div>
+                            <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-purple-500" href="">Excalidraw Rice</Link>
+                            <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-red-500" href="">Invidious</Link>
+                            <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-orange-500" href="">Redlib</Link>
+                            <Link className="flex justify-center text-2xl mx-10 my-5 rounded-sm bg-gray-800 text-green-500" href="">Personal CTFd</Link>
                         </div> 
                         <div className="flex-grow font-sourgummy text-white bg-black border-2 rounded-lg w-full mb-[6%]">
                             
