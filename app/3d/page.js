89cecf312a7f38704341a08dc6e0ecb7465dc105
yoshixi <davidchenyuhe@proton.me>
2025-05-04 22:15:16 -0400

    cube at.. erm bottom + ctfd link

--- app/3d/page.js
+++ app/3d/page.js
@@ -14,7 +14,6 @@ export default function Page() {
   return (
     <PageLayout title="3D">
       <div className="justify-center justify-items-center grid grid-cols-6 gap-4">
-        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/cube.glb`} pos={{x:0,y:0,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
         <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/duelist.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
         <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/mercenary.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
         <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/ranger.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
@@ -27,6 +26,7 @@ export default function Page() {
         <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/commandant.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
         <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/alchemist.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
         <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/cultist.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/cube.glb`} pos={{x:0,y:0,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
       </div>
     </PageLayout>
   )
