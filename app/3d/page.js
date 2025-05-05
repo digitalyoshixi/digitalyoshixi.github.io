2dc1d270bd4f25bc47247bec80197da91b00603a
yoshixi <davidchenyuhe@proton.me>
2025-05-04 21:29:16 -0400

    new three dee models

--- app/3d/page.js
+++ app/3d/page.js
@@ -5,11 +5,21 @@ import ModelCanvas from "../components/canvas"
 export default function Page() {
   return (
     <PageLayout title="3D">
-    <div className=" bg-black flex-none rounded-lg h-40 w-40">
-      <ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40">
-          <GeneralModel path="/3d/cube.glb" rot={{x : 0, y : 0, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel>
-      </ModelCanvas>
+    <div className="grid grid-cols-4 gap-4">
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/duelist.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/mercenary.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/ranger.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/shoat.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/magician.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/halberdier.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/headhunter.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/salamander.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/bombardier.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/commandant.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/alchemist.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
+    <div className=" bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas className=" bg-black flex-none rounded-lg h-40 w-40"><GeneralModel path="/3d/cultist.glb" pos={{x:-1,y:-2.6,z:0}} rot={{x : 0.2, y : -1.7, z : 0}} scale={{x : 1, y : 1, z : 1}}></GeneralModel></ModelCanvas></div>
     </div>
+
 
     </PageLayout>
   )
--- app/components/generalview.js
+++ app/components/generalview.js
@@ -1,15 +1,15 @@
 'use client'
 import ModelCanvas from './canvas'
 import { OrbitControls, useGLTF , View} from '@react-three/drei';
-function Model({ path , rot, scale}) {
+function Model({ path , pos, rot, scale}) {
   const { scene } = useGLTF(path);
-  return <primitive object={scene} rotation={[rot.x,rot.y,rot.z]} scale={[scale.x, scale.y, scale.z]} />;
+  return <primitive object={scene} position={[pos.x, pos.y, pos.z]} rotation={[rot.x,rot.y,rot.z]} scale={[scale.x, scale.y, scale.z]} />;
 };
 
-export default function GeneralModel({path, rot, scale}) {
+export default function GeneralModel({path, pos, rot, scale}) {
   return (
     <>
-        <Model path={path} rot={rot} scale={scale}/>
+        <Model path={path} pos={pos} rot={rot} scale={scale}/>
         <OrbitControls />
     </>
   );
