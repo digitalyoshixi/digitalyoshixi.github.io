4438b75e39d75f2ca34fb0012b10dc70b4ac97a4
yoshixi <davidchenyuhe@proton.me>
2025-05-04 22:29:35 -0400

    test speed2

--- app/3d/page.js
+++ app/3d/page.js
@@ -1,12 +1,11 @@
 'use client'
 import PageLayout from "../components/pagelayout"
 import GeneralModel from "../components/generalview"
-import { Canvas } from "@react-three/fiber"
-import { Suspense } from "react"
+import ModelCanvas from "../components/canvas"
 import { OrbitControls } from "@react-three/drei"
 
 const models = [
-  "duelist", "mercenary", "ranger", "shoat", "magician", 
+  "cube", "duelist", "mercenary", "ranger", "shoat", "magician", 
   "halberdier", "headhunter", "salamander", "bombardier",
   "commandant", "alchemist", "cultist"
 ]
@@ -14,25 +13,20 @@ const models = [
 export default function Page() {
   return (
     <PageLayout title="3D">
-      <div className="justify-center justify-items-center grid grid-cols-4 gap-4">
-        {models.map((model, index) => (
-          <div key={model} className="bg-black m-2 flex-none rounded-lg h-40 w-40">
-            <Canvas>
-              <Suspense fallback={null}>
-                <directionalLight position={[10, 10, 10]} intensity={3} />
-                <directionalLight position={[10, -10, -10]} intensity={3} />
-                <directionalLight position={[-10, 0, 0]} intensity={3} />
-                <GeneralModel 
-                  path={`/3d/${model}.glb`}
-                  pos={{x:-1,y:-2.6,z:0}}
-                  rot={{x:0.2,y:-1.7,z:0}}
-                  scale={{x:1,y:1,z:1}}
-                />
-                <OrbitControls makeDefault />
-              </Suspense>
-            </Canvas>
-          </div>
-        ))}
+      <div className="justify-center justify-items-center grid grid-cols-6 gap-4">
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/duelist.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/mercenary.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/ranger.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/shoat.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/magician.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/halberdier.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/headhunter.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/salamander.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/bombardier.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/commandant.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/alchemist.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        udiv className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/cultist.glb`} pos={{x:-1,y:-2.6,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
+        <div className="bg-black m-2 flex-none rounded-lg h-40 w-40"><ModelCanvas><GeneralModel path={`/3d/cube.glb`} pos={{x:0,y:0,z:0}} rot={{x:0.2,y:-1.7,z:0}} scale={{x:1,y:1,z:1}}/><OrbitControls makeDefault /></ModelCanvas></div>
       </div>
     </PageLayout>
   )
